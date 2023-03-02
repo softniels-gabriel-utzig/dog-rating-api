c3ff0f1c48e90d655e02601db79b68ffacae500d
Utzig <gabriel.utzig26@gmail.com>
2023-03-02 16:57:46 -0300

    project init

---
+++ DogStandalone.dpr
@@ -0,0 +1,73 @@
+program DogStandalone;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  MVCFramework,
+  MVCFramework.Logger,
+  MVCFramework.Commons,
+  MVCFramework.Signal,
+  Web.ReqMulti,
+  Web.WebReq,
+  Web.WebBroker,
+  IdContext,
+  IdHTTPWebBrokerBridge,
+  Dog.Controller in 'Dog.Controller.pas',
+  Main.WebModule in 'Main.WebModule.pas' {DogRating: TWebModule},
+  Dog.Entity in 'Dog.Entity.pas',
+  Data.Connection in 'Data.Connection.pas' {DataStream: TDataModule},
+  Dog.DAO in 'Dog.DAO.pas',
+  Rate.entity in 'Rate.entity.pas',
+  Rate.DAO in 'Rate.DAO.pas';
+
+{$R *.res}
+
+
+procedure RunServer(APort: Integer);
+var
+  LServer: TIdHTTPWebBrokerBridge;
+begin
+  Writeln('** DMVCFramework Server ** build ' + DMVCFRAMEWORK_VERSION);
+  LServer := TIdHTTPWebBrokerBridge.Create(nil);
+  try
+    LServer.OnParseAuthentication := TMVCParseAuthentication.OnParseAuthentication;
+    LServer.DefaultPort := APort;
+    LServer.KeepAlive := True;
+
+    { more info about MaxConnections
+      http://ww2.indyproject.org/docsite/html/frames.html?frmname=topic&frmfile=index.html }
+    LServer.MaxConnections := 0;
+
+    { more info about ListenQueue
+      http://ww2.indyproject.org/docsite/html/frames.html?frmname=topic&frmfile=index.html }
+    LServer.ListenQueue := 200;
+
+    LServer.Active := True;
+    WriteLn('Listening on port ', APort);
+    Write('CTRL+C to shutdown the server');
+    WaitForTerminationSignal;
+    EnterInShutdownState;
+    LServer.Active := False;
+  finally
+    LServer.Free;
+  end;
+end;
+
+begin
+  ReportMemoryLeaksOnShutdown := True;
+  IsMultiThread := True;
+  // DMVCFramework Specific Configuration
+  // When MVCSerializeNulls = True empty nullables and nil are serialized as json null.
+  // When MVCSerializeNulls = False empty nullables and nil are not serialized at all.
+  MVCSerializeNulls := True;
+  try
+    if WebRequestHandler <> nil then
+      WebRequestHandler.WebModuleClass := WebModuleClass;
+    WebRequestHandlerProc.MaxConnections := 1024;
+    RunServer(8080);
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
